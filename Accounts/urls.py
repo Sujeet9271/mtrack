5f3e6f3122752ecf1d127339dc61bd0ae51db3d7
Sujeet Kumar Gupta <sujeet9271@gmail.com>
2021-01-23 18:52:38 +0545

    Add profile page

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login, name='auth_user'),
     path('register/', views.register, name='register'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('logout/', views._logout, name='logout')
+    path('logout/', views._logout, name='logout'),
+    path('profile/',views.profile, name='profile')
 ]
