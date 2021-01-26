cc58ac7a7808babf48b53bd4245267c73631fe00
Sujeet Kumar Gupta <sujeet9271@gmail.com>
2021-01-26 15:10:29 +0545

    Adding Profile Page

--- Accounts/models.py
+++ Accounts/models.py
@@ -7,3 +7,12 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=20, null=True)
     work = models.CharField(max_length=50, null=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            profile, created = Profile.objects.get_or_create(user=instance)
+
+    post_save.connect(create_user_profile, sender=User)
+
+    def __str__(self):
+        return str(self.user)
