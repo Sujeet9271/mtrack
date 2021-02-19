4bbea36be50db1f992a9d85abce0529dc1b08907
Sujeet <sujeet9271@gmail.com>
2021-02-19 19:57:59 +0545

    changing chart type

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,50 +1,4 @@
-const renderdetailChart=(amount,sources) => {
-              var ctx = document.getElementById('details').getContext('2d');
-              var detailChart = new Chart(ctx, {
-                  type: 'doughnut',
-                  data: {
-                      labels:sources,
-                      datasets: [{
-                          label: 'Money Detail',
-                          data:amount ,
-                          backgroundColor: [
-                              'rgba(255, 99, 132, 0.5)',
-                              'rgba(54, 162, 235, 0.5)',
-                              'rgba(255, 206, 86, 0.5)',
-                              'rgba(75, 192, 192, 0.5)',
-                              'rgba(153, 102, 255, 0.5)',
-                              'rgba(255, 159, 64, 0.5)'
-                          ],
-                          borderColor: [
-                              'rgba(255, 99, 132, 1)',
-                              'rgba(54, 162, 235, 1)',
-                              'rgba(255, 206, 86, 1)',
-                              'rgba(75, 192, 192, 1)',
-                              'rgba(153, 102, 255, 1)',
-                              'rgba(255, 159, 64, 1)'
-                          ],
-                          borderWidth: 2
-                      }]
-                  },
-                  options: {
-                      title:{
-                        display:true,
-                        text:'Money Details'
-                      },
-                      legend:{
-                          display:true,
-                          text:'Money Detail',
-                          position:'bottom',
-                          align:'center'                     
-                        
 
-                      },
-                      responsive:true,
-                      maintainAspectRatio:false
-                      
-                  },                
-              });
-      }
 
 const renderexpenseChart=(amount,category) => {
         var ctx = document.getElementById('expenseChart').getContext('2d');
@@ -141,3 +95,118 @@ const renderincomeChart=(amount,source) => {
         },                
     });
 }
+
+
+
+
+
+
+const renderdetailChart=(expense_label,expense_data,income_data,income_label,savings_data,savings_label) => {
+  var ctx = document.getElementById('details').getContext('2d');
+  var detailChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: details,
+      options: chartOptions
+    })
+ 
+  var income = {
+      label: 'income',
+      data: income_data,
+      backgroundColor: 'green',
+      borderWidth: 0,
+      yAxisID: "y-axis-density"
+    };
+    
+  var expense = {
+      label: 'expense',
+      data: expense_data,
+      backgroundColor: 'red',
+      borderWidth: 0,
+      yAxisID: "y-axis-gravity"
+    };
+    
+  var savings = {
+      label: 'savings',
+      data: savings_data,
+      backgroundColor: 'yellow',
+      borderWidth: 0,
+      yAxisID: "y-axis-gravity"
+    };
+    
+  var details = {
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      datasets: [income,expense,savings]
+    };
+    
+  var chartOptions = {
+      scales: {
+        xAxes: [{
+          barPercentage: 1,
+          categoryPercentage: 0.6
+        }],
+        yAxes: [{
+          id: "y-axis-density"
+        }, {
+          id: "y-axis-gravity"
+        }]
+      }
+    };
+    
+  };
+
+
+
+//exp
+  const renderdetailChart=(expense_label,expense_data,income_data,income_label,savings_data,savings_label) => {
+    var ctx = document.getElementById('details').getContext('2d');
+    var detailChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: details,
+        options: chartOptions
+      })
+   
+    var income = {
+        label: 'income',
+        data: income_data,
+        backgroundColor: 'green',
+        borderWidth: 0,
+        yAxisID: "y-axis-density"
+      };
+      
+    var expense = {
+        label: 'expense',
+        data: expense_data,
+        backgroundColor: 'red',
+        borderWidth: 0,
+        yAxisID: "y-axis-gravity"
+      };
+      
+    var savings = {
+        label: 'savings',
+        data: savings_data,
+        backgroundColor: 'yellow',
+        borderWidth: 0,
+        yAxisID: "y-axis-gravity"
+      };
+      
+    var details = {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        datasets: [income,expense,savings]
+      };
+      
+    var chartOptions = {
+        scales: {
+          xAxes: [{
+            barPercentage: 1,
+            categoryPercentage: 0.6
+          }],
+          yAxes: [{
+            id: "y-axis-density"
+          }, {
+            id: "y-axis-gravity"
+          }]
+        }
+      };
+      
+    };
+  
