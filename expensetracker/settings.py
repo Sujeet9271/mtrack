4db21f966a9a05d319fea41aa6082d17b2507d91
Sujeet Kumar Gupta <56026642+Sujeet9271@users.noreply.github.com>
2021-03-25 21:12:09 +0545

    secret key update

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')=r&ymx4dum38wax9fo!1jdcvql+_^t72#awhdwug+0qh8s-qy'
+SECRET_KEY = '&ymx4dum38wax9fo!1jdcvql+_^t72#awhdwug+0qh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
